7390e478ff4e871017030577b4309f7cb24460cb
IZAWA, Kanji <5319055+kanjiz@users.noreply.github.com>
2023-06-22 15:35:56 +0900

    initial commit

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -1,11 +1,17 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class HelloTest {
   @Test
   public void testMain() {
     Hello.main(null);
     assertTrue(true);
+  }
+
+  @Test
+  public void testFail() {
+    fail();
   }
 }
