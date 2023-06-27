a714930b73b4b3cd0643d45f5f74a6d32b269675
IZAWA, Kanji <5319055+kanjiz@users.noreply.github.com>
2023-06-27 10:53:36 +0900

    initial commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,12 +16,16 @@ tasks.withType<JavaCompile>().configureEach {
   options.encoding = "UTF-8"
 }
 
+// `test`タスクの設定
 tasks.test {
+  // JUnit 5を使用するように設定
   useJUnitPlatform()
   testLogging {
+    // ログに表示するイベントを指定
     events("passed", "skipped", "failed")
     outputs.upToDateWhen { false }
     showCauses = true
+    // 標準出力・標準エラー出力を表示
     showStandardStreams = true
   }
 }
