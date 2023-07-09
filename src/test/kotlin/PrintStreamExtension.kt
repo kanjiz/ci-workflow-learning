97217360afc645a819a8faf890ba44ac6505d32b
kanjiz <5319055+kanjiz@users.noreply.github.com>
2023-07-10 01:47:28 +0900

    initial commit

--- src/test/kotlin/PrintStreamExtension.kt
+++ src/test/kotlin/PrintStreamExtension.kt
@@ -1,51 +1,68 @@
-import org.junit.jupiter.api.extension.AfterEachCallback
-import org.junit.jupiter.api.extension.BeforeEachCallback
-import org.junit.jupiter.api.extension.ExtensionContext
+import io.kotest.core.listeners.AfterEachListener
+import io.kotest.core.listeners.BeforeEachListener
+import io.kotest.core.test.TestCase
+import io.kotest.core.test.TestResult
 import java.io.ByteArrayOutputStream
 import java.io.PrintStream
 
 /**
- * JUnit 5の拡張機能を使用して、System.outの出力をキャプチャするためのユーティリティクラスです。
+ * System.outの出力をキャプチャするための拡張クラスです。
  *
- * このクラスを使用すると、テストメソッドの実行前にSystem.outの出力をキャプチャし、 テストメソッドの実行後にSystem.outの出力を元に戻します。
+ * このクラスは、Kotestの `BeforeEachListener` と `AfterEachListener` インターフェースを実装しています。
+ * これにより、テストメソッドの実行前にSystem.outの出力をキャプチャし、テストメソッドの実行後にSystem.outの出力を元に戻すことができます。
  *
- * また、キャプチャされた出力を取得するためのメソッドも提供しています。
+ * また、このクラスは、キャプチャされた出力を取得するための `getOutput()` メソッドを提供しています。
  *
- * @see org.junit.jupiter.api.extension.Extension
- * @see org.junit.jupiter.api.extension.BeforeEachCallback
- * @see org.junit.jupiter.api.extension.AfterEachCallback
- * @see java.io.ByteArrayOutputStream
- * @see java.io.PrintStream
+ * このクラスを使用する場合、`extension` メソッドを呼び出す必要があります。
+ * 例えば、以下のように呼び出します。
+ *
+ * 例:
+ * ```
+ * class Chapter01Test : FunSpec({
+ *  context("第一章") {
+ *  val outputCaptor = PrintStreamExtension()
+ *  extension(outputCaptor)
+ *  ...
  */
-class PrintStreamExtension : BeforeEachCallback, AfterEachCallback {
-  /** System.outの初期値を保持する変数です。 */
+class PrintStreamExtension() : BeforeEachListener, AfterEachListener {
+
+  /**
+   * System.outの初期値を保持する変数です。
+   */
   private val out = System.out
 
-  /** System.outの出力をキャプチャするためのByteArrayOutputStreamです。 */
+  /**
+   * System.outの出力をキャプチャするためのByteArrayOutputStreamです。
+   */
   private val bytes = ByteArrayOutputStream()
 
   /**
-   * テストメソッドの前に実行されるメソッドです。 System.outを一時的にByteArrayOutputStreamにリダイレクトします。
-   * @param context テストメソッドのコンテキスト
-   */
-  override fun beforeEach(context: ExtensionContext) {
-    System.setOut(PrintStream(bytes))
-  }
-
-  /**
-   * テストメソッドの後に実行されるメソッドです。 System.outを元の出力に戻し、ByteArrayOutputStreamをリセットします。
-   * @param context テストメソッドのコンテキスト
-   */
-  override fun afterEach(context: ExtensionContext) {
-    System.setOut(out)
-    bytes.reset()
-  }
-
-  /**
    * ByteArrayOutputStreamにキャプチャされた出力を取得するためのメソッドです。
+   *
    * @return ByteArrayOutputStreamにキャプチャされた出力
    */
   fun getOutput(): String {
     return bytes.toString().trim()
+  }
+
+  /**
+   * テストメソッドの実行前にSystem.outの出力をキャプチャするためのメソッドです。
+   *
+   * @param testCase テストケース
+   */
+  override suspend fun beforeEach(testCase: TestCase) {
+    super.beforeEach(testCase)
+    System.setOut(PrintStream(bytes))
+  }
+
+  /**
+   * テストメソッドの実行後にSystem.outの出力を元に戻すためのメソッドです。
+   * @param testCase テストケース
+   * @param result テスト結果
+   */
+  override suspend fun afterEach(testCase: TestCase, result: TestResult) {
+    super.afterEach(testCase, result)
+    System.setOut(out)
+    bytes.reset()
   }
 }
--- src/test/kotlin/chapter01/Chapter01Test.kt
+++ src/test/kotlin/chapter01/Chapter01Test.kt
@@ -3,41 +3,22 @@ package chapter01
 import PrintStreamExtension
 import TestUtils
 import io.kotest.core.spec.style.FunSpec
-import io.kotest.matchers.shouldBe
-import java.io.ByteArrayOutputStream
-import java.io.PrintStream
 
 class Chapter01Test : FunSpec({
   context("第一章") {
     val outputCaptor = PrintStreamExtension()
-    val bytes = ByteArrayOutputStream()
-    val out = System.out
-    beforeEach {
-      println("beforeEach")
-      System.setOut(PrintStream(bytes))
-    }
-
-    afterEach {
-      System.setOut(out)
-      bytes.reset()
-      println("afterEach")
-    }
+    extension(outputCaptor)
 
     test("Helloクラスのmainメソッドのテスト") {
       Hello.main(emptyArray())
       val expectedOutput = "Hello!"
-//    TestUtils.assertOutput(outputCaptor.getOutput(), expectedOutput)
-
-      TestUtils.assertOutput(bytes.toString().trim(), expectedOutput)
-//    bytes.toString().trim() shouldBe expectedOutput
+      TestUtils.assertOutput(outputCaptor.getOutput(), expectedOutput)
     }
 
     test("HelloJavaクラスのmainメソッドのテスト") {
       HelloJava.main(emptyArray())
       val expectedOutput = "Hello Java world"
-//    TestUtils.assertOutput(outputCaptor.getOutput(), expectedOutput)
-    TestUtils.assertOutput(bytes.toString().trim(), expectedOutput)
-//      bytes.toString().trim() shouldBe expectedOutput
+      TestUtils.assertOutput(outputCaptor.getOutput(), expectedOutput)
     }
   }
 
