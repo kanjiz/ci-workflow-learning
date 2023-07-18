46d4bc56267c5878f67f9e0a3bbb95019bb50a47
kanjiz <5319055+kanjiz@users.noreply.github.com>
2023-07-18 16:55:39 +0900

    initial commit

--- src/test/kotlin/utility/ParameterData.kt
+++ src/test/kotlin/utility/ParameterData.kt
@@ -3,4 +3,13 @@ package utility
 data class ParameterData(
   val name: String,
   val type: String
-)
+) {
+  init {
+    require(name.isBlank()) { "Parameter name is blank." }
+    require(type.isBlank()) { "Parameter type is blank." }
+  }
+
+  override fun toString(): String {
+    return "$name: $type"
+  }
+}
