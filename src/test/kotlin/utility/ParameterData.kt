26e7e17bc852fe7917c918cc86a3913d53f779b7
kanjiz <5319055+kanjiz@users.noreply.github.com>
2023-07-18 16:58:40 +0900

    initial commit

--- src/test/kotlin/utility/ParameterData.kt
+++ src/test/kotlin/utility/ParameterData.kt
@@ -1,9 +1,12 @@
 package utility
 
-data class ParameterData(
-  val name: String,
-  val type: String
-) {
+/**
+ * パラメータの名前と型を表すデータクラス。
+ *
+ * @property name パラメータの名前
+ * @property type パラメータの型
+ */
+data class ParameterData(val name: String, val type: String) {
   init {
     require(name.isBlank()) { "Parameter name is blank." }
     require(type.isBlank()) { "Parameter type is blank." }
