87d28656349ac3ebe19c272c02ca9c475017846a
kanjiz <5319055+kanjiz@users.noreply.github.com>
2023-07-26 15:06:20 +0900

    initial commit

---
+++ src/main/java/constax/ConsTax.java
@@ -0,0 +1,20 @@
+/*
+ * 消費税込みの金額を求めるプログラム
+ */
+
+import java.io.*;
+
+public class ConsTax {
+  public static void main(String[] args) throws IOException {
+    int money; // 金額入力のエリア
+
+    /* 入力の準備 */
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    /* 税込み金額の算出と表示 */
+    System.out.println("金額を入力してください");
+    money = Integer.parseInt(br.readLine()); // 金額の入力
+    money *= 1.08; // 税込み金額を求める
+    System.out.println("税込み\\" + +money); // 税込み金額を表示する
+  }
+}
